acd8f88f19bb844ff81ce120264a1de049beeaef
root <root@54208e76e50b>
2017-04-18 17:40:25 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "HelloWorld",
+  "homePage" : "Main",
+  "name" : "HelloWorld",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
